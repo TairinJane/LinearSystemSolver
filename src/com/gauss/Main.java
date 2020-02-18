5c598a6467f5188ce43f46b4fb524128cd23772c
Tairin <tairinjane@gmail.com>
2020-02-18 12:24:07 +0300

    Added copy and random methods Formatted output

--- src/com/gauss/Main.java
+++ src/com/gauss/Main.java
@@ -1,6 +1,7 @@
 package com.gauss;
 
 import java.io.*;
+import java.util.Random;
 
 public class Main {
 
@@ -9,7 +10,7 @@ public class Main {
     private static void printMatrix(double[][] a) {
         for (double[] row : a) {
             for (double c : row) {
-                System.out.print(c + "\t");
+                System.out.print(String.format("%12.5f", c));
             }
             System.out.println();
         }
@@ -54,7 +55,7 @@ public class Main {
             for (int j = 0; j < a.length; j++) {
                 a[j][i] *= solution[i];
             }
-            System.out.println("x" + (i + 1) + " = " + solution[i]);
+            System.out.println(String.format("x%d = %10.5f", (i + 1), solution[i]));
         }
         return solution;
     }
@@ -66,7 +67,7 @@ public class Main {
             for (int j = 0; j < a.length; j++) {
                 rowSum += a[i][j] * solution[j];
             }
-            System.out.println("Row " + (i + 1) + ": " + rowSum + " = " + a[i][a[i].length - 1]);
+            System.out.println(String.format("Row %d: %10.5f = %10.5f", (i + 1), rowSum, a[i][a[i].length - 1]));
             if (Math.abs(rowSum - a[i][a[i].length - 1]) > EPSILON) return false;
         }
         return true;
@@ -101,17 +102,37 @@ public class Main {
         a[j] = temp;
     }
 
+    private static double[][] randomMatrix(int size) {
+        double[][] matrix = new double[size][size + 1];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size + 1; j++) {
+                matrix[i][j] = Math.random() * 100;
+            }
+        }
+        return matrix;
+    }
+
+    private static double[][] copyMatrix(double[][] matrix) {
+        int size = matrix.length;
+        double[][] copy = new double[size][size + 1];
+        for (int i = 0; i < size; i++) {
+            copy[i] = matrix[i].clone();
+        }
+        return copy;
+    }
+
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         int size = 0;
         System.out.println("Your matrix size (N x N):");
-        while (size == 0) {
+        while (size <= 0) {
             try {
                 size = Integer.parseInt(reader.readLine());
+                if (size <= 0) throw new NumberFormatException();
                 System.out.println("Size: " + size + " x " + size);
             } catch (Exception e) {
-                System.out.println("Enter correct integer number, please.");
+                System.out.println("Enter correct positive integer number, please.");
             }
         }
         System.out.println();
@@ -119,13 +140,15 @@ public class Main {
         System.out.println("Do you want to use console (c) or file (f) for input?");
         boolean useFile = false;
         boolean inputSource = false;
+        boolean random = false;
         while (!inputSource) {
             try {
                 String answer = reader.readLine().trim();
-                if (!(answer.equals("c") || answer.equals("f")))
+                if (!(answer.equals("c") || answer.equals("f") || answer.equals("r")))
                     System.out.println("Enter c to use console, f - to use file");
                 else {
                     useFile = answer.equals("f");
+                    random = answer.equals("r");
                     inputSource = true;
                 }
             } catch (IOException e) {
@@ -135,7 +158,6 @@ public class Main {
         System.out.println();
 
         double[][] matrix = new double[size][size + 1];
-        double[][] checkMatrix = new double[size][size + 1];
 
         if (useFile) {
             BufferedReader fileReader;
@@ -153,7 +175,6 @@ public class Main {
                                         .replaceAll("[ ]+", " ").split(" ");
                                 for (int j = 0; j < size + 1; j++) {
                                     matrix[i][j] = Double.parseDouble(row[j]);
-                                    checkMatrix[i][j] = Double.parseDouble(row[j]);
                                 }
                             }
                             gotFile = true;
@@ -167,6 +188,8 @@ public class Main {
                 System.out.println("Invalid file path, try again");
             }
 
+        } else if (random) {
+            matrix = randomMatrix(size);
         } else {
 
             System.out.println("Enter all matrix coefficients row by row:");
@@ -186,7 +209,6 @@ public class Main {
                                 .replaceAll("[ ]+", " ").split(" ");
                         for (int j = 0; j < size + 1; j++) {
                             matrix[i][j] = Double.parseDouble(row[j]);
-                            checkMatrix[i][j] = Double.parseDouble(row[j]);
                         }
                         nextRow = true;
                     } catch (Exception e) {
@@ -200,10 +222,12 @@ public class Main {
         System.out.println("Your matrix:");
         printMatrix(matrix);
 
+        double[][] checkMatrix = copyMatrix(matrix);
+
         forwardGauss(matrix);
 
         double det = determinant(matrix);
-        System.out.println("Matrix determinant: " + det);
+        System.out.println(String.format("Matrix determinant: %10.5f", det));
         System.out.println();
 
         if (det == 0) {
